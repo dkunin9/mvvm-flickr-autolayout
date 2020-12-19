151e0db0623ace66725c4dc4be51405065960eca
daniel <kuningram@gmail.com>
2020-12-19 14:47:03 +0300

    Initial Commit

---
+++ mvvm-flickr-autolayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvvm-flickr-autolayout
+//
+//  Created by Daniel on 19.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
