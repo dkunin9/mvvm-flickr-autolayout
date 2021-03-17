c8e0b20d0127b030fa57b6d9a63a0ff37663be4a
Daniil <kuningram@gmail.com>
2021-03-17 17:12:37 +0300

    Unit tests for Models added

---
+++ mvvm-flickr-autolayoutTests/Models/SearchGroupTests.swift
@@ -0,0 +1,33 @@
+//
+//  SearchImagesTests.swift
+//  mvvm-flickr-autolayout
+//
+//  Created by Daniel on 17.03.2021.
+//
+
+import XCTest
+@testable import mvvm_flickr_autolayout
+
+class SearchGroupTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    //MARK: Test - SearchGroup Model from Data.json
+    
+    func testFlickrPhotosJSONDecoder() throws {
+        let bundle = Bundle(for: type(of: self))
+        let fileUrl = bundle.url(forResource: "Data", withExtension: "json")!
+        let data = try! Data(contentsOf: fileUrl)
+        let dictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
+        let searchGroup = SearchGroup(with: dictionary!)
+        XCTAssertTrue(searchGroup.page == 1)
+        XCTAssertTrue(searchGroup.perPage == 20)
+    }
+
+}
---
+++ mvvm-flickr-autolayoutTests/Models/SearchResultTests.swift
@@ -0,0 +1,44 @@
+//
+//  SearchResultTests.swift
+//  mvvm-flickr-autolayoutTests
+//
+//  Created by Daniel on 17.03.2021.
+//
+
+import XCTest
+@testable import mvvm_flickr_autolayout
+
+class SearchResultTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    //MARK: Test - SearchResult Model from Data.json
+    
+    func testFlickrPhotosJSONDecoder() throws {
+        let searchGroup = getSearchGroup()
+        let searchResults = searchGroup.searchResults as! [FlickrPhoto]
+        XCTAssertNotNil(searchResults)
+        let flickrPhoto = searchResults[0]
+        XCTAssertTrue(flickrPhoto.id == "7432654189")
+        XCTAssertFalse(flickrPhoto.thumbnailURL == nil)
+        XCTAssertEqual(flickrPhoto.title, "Moon")
+        XCTAssertEqual(flickrPhoto.ownerID, "user12312")
+    }
+    
+    
+    func getSearchGroup() -> SearchGroup {
+        let bundle = Bundle(for: type(of: self))
+        let fileUrl = bundle.url(forResource: "Data", withExtension: "json")!
+        let data = try! Data(contentsOf: fileUrl)
+        let dictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
+        let searchGroup = SearchGroup(with: dictionary!)
+        return searchGroup
+    }
+
+}
